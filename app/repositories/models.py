20a31956ad263cd3e07136a06421b12d0fdd2710
Nazareno Rodríguez <nazarenoorodriguez@gmail.com>
2023-06-16 11:30:45 -0300

    refactor: Modify the database so no warnings show up anymore

--- app/repositories/models.py
+++ app/repositories/models.py
@@ -1,47 +1,46 @@
 from datetime import datetime
 from app.plugins import db
 
+order_x_ingredient = db.Table(
+    'order_x_ingredient',
+    db.Column('order_id', db.Integer, db.ForeignKey('order._id'), primary_key=True),
+    db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredient._id'), primary_key=True)
+)
+
+order_x_beverage = db.Table(
+    'order_x_beverage',
+    db.Column('order_id', db.Integer, db.ForeignKey('order._id'), primary_key=True),
+    db.Column('beverage_id', db.Integer, db.ForeignKey('beverage._id'), primary_key=True)
+)
+
 class Ingredient(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
+    orders = db.relationship('Order', secondary=order_x_ingredient, back_populates='ingredients')
 
 class Beverage(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
+    orders = db.relationship('Order', secondary=order_x_beverage, back_populates='beverages')
 
 class Size(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
-
-class OrderXIngredient(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('order._id'))
-    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient._id'))
-    order = db.relationship('Order', backref=db.backref('order_ingredients', overlaps="order,order_ingredients"))
-    ingredient = db.relationship('Ingredient', backref=db.backref('ingredient'))
-
-class OrderXBeverage(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('order._id'))
-    beverage_id = db.Column(db.Integer, db.ForeignKey('beverage._id'))
-    order = db.relationship('Order', backref=db.backref('order_beverages', overlaps="order,order_beverages"))
-    beverage = db.relationship('Beverage', backref=db.backref('beverage'))
+    orders = db.relationship('Order', back_populates='size')
 
 class Order(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, default=datetime.utcnow)
     size_id = db.Column(db.Integer, db.ForeignKey('size._id'))
-    size = db.relationship('Size', backref=db.backref('size'))
-    ingredients = db.relationship('Ingredient', secondary='order_x_ingredient', backref=db.backref('orders', lazy=True))
-    beverages = db.relationship('Beverage', secondary='order_x_beverage', backref=db.backref('orders', lazy=True))
+    size = db.relationship('Size', back_populates='orders')
+    ingredients = db.relationship('Ingredient', secondary=order_x_ingredient, back_populates='orders')
+    beverages = db.relationship('Beverage', secondary=order_x_beverage, back_populates='orders')
     total_price = db.Column(db.Float)
-    order_x_ingredient = db.relationship('OrderXIngredient', backref=db.backref('order_x_ingredient'))
-    order_x_beverage = db.relationship('OrderXBeverage', backref=db.backref('order_x_beverage'))
     customer_id = db.Column(db.Integer, db.ForeignKey('customer._id'))
-    customer = db.relationship('Customer', backref=db.backref('orders_customer'))
+    customer = db.relationship('Customer', back_populates='orders')
 
 class Customer(db.Model):
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -49,22 +48,4 @@ class Customer(db.Model):
     client_dni = db.Column(db.String(10))
     client_address = db.Column(db.String(128))
     client_phone = db.Column(db.String(15))
-    orders = db.relationship('Order', backref=db.backref('customer_orders'))
-    
-
-class Report(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
-    most_requested_ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient._id'))
-    most_requested_ingredient = db.relationship('Ingredient', backref=db.backref('report'))
-    year = db.Column(db.Integer, default=datetime.now().year)
-    month_with_most_revenue = db.Column(db.String(15), nullable=False)
-    sales_in_month_with_most_revenue = db.Column(db.Float, nullable=False)
-    top_one_customer_id = db.Column(db.Integer, db.ForeignKey('customer._id'))
-    top_two_customer_id = db.Column(db.Integer, db.ForeignKey('customer._id'))
-    top_three_customer_id = db.Column(db.Integer, db.ForeignKey('customer._id'))
-    top_one_customer = db.relationship('Customer', foreign_keys=[top_one_customer_id], backref='top_one_reports')
-    top_two_customer = db.relationship('Customer', foreign_keys=[top_two_customer_id], backref='top_two_reports')
-    top_three_customer = db.relationship('Customer', foreign_keys=[top_three_customer_id], backref='top_three_reports')
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    
+    orders = db.relationship('Order', back_populates='customer')
